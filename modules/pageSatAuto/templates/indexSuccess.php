17670b18ced2b94c66a5299b4b18c5f58dd0dfa4
Nicolas Ricci <contact@nrsoft.co.uk>
2013-06-05 13:19:23 +0100

    - git submodules - tinymce - import csv

--- modules/pageSatAuto/templates/indexSuccess.php
+++
@@ -1,8 +0,0 @@
-<h1>Liste des pages satelittes</h1>
-<ul>
-    <?php foreach ($page_sats as $page_sat): ?>
-    <li>
-        <?php echo link_to($page_sat->keyword, '@pix_page_sat_auto_show?slug='.$page_sat->slug); ?>
-    </li>
-    <?php endforeach; ?>
-</ul>
--- modules/pageSatAuto/templates/showSuccess.php
+++
@@ -1,2 +0,0 @@
-<h1><?php echo $page_sat->getTitle(); ?></h1>
-<?php echo $page_sat->getContent(ESC_RAW); ?>
